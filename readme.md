2bc6121fb6b2d2a0e521ac82cf0ba8ba3cd6fc1a
Dominic Shooter <dominic.shooter@lic.co.nz>
2019-05-21 15:36:12 +1200

    ui initital commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+#Getting started
+
+##Quick start
+
+### Run react app
+```cd ux-lic
+npm start```
+
+Then open http://localhost:3000/ to see your app.
+
+### Run
---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bulma/css/bulma.css';
+import './mytheme.css';
+import { Button } from 'trunx'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <div className="container">
+
+          <div className="app-grid">
+
+            <div className="app-heading">
+              <div className="app-heading-title">
+                <h1 className="is-size-1">Typography</h1>
+              </div>
+            </div>
+            <div className="content">
+              <h1>Heading 1</h1>
+              <h2>Heading 2</h2>
+              <h3>Heading 3</h3>
+              <h4>Heading 4</h4>
+              <p>Paragraph with some small text inside</p>
+              <label>label</label>
+
+              <p className="lead">The system font for LIC public facing sites is Booster Next FY. Booster Next FY is a friendly, round, sans-serif typeface that requires a license to install. </p>
+              <p className="lead">To ensure we have consistent and predictable sizing in all browsers we use a root (base) font size of 16px. We use an rem unit is relative to the root element. That means that we can define a single font size on the html element and define all rem units to be a percentage of that. </p>
+            </div>
+
+            <div className="app-heading">
+              <div className="app-heading-title">
+                <h1 className="is-size-1">Icons</h1>
+              </div>
+            </div>
+
+            <div className="app-heading">
+              <div className="app-heading-title">
+                <h1 className="is-size-1">Buttons</h1>
+              </div>
+            </div>
+
+            <div className="app-subheading">
+                <h4 className="is-size-4">Simple buttons</h4>
+            </div>
+
+
+
+            <div className="app-grid app-grid-tight">
+                
+                  <Button small outlined>Outlined</Button>
+                  <Button isPrimary>Primary</Button>
+                  <Button isInfo>Seconary</Button>
+                  <Button isText>Link</Button>
+            </div>
+
+            <div className="app-grid app-grid-tight">
+                
+                  <Button isSmall outlined>Outlined</Button>
+                  <Button isSmall isPrimary>Primary</Button>
+                  <Button isSmall isInfo>Seconary</Button>
+            </div>
+
+          </div>
+
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
